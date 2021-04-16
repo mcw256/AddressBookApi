45099b9102840af7e1d0a7b96ea50cab7305a7d8
mcw256 <rivael256@gmail.com>
2021-04-16 22:55:55 +0200

    added memory cache service

---
+++ AddressBookApi/Services/IMemoryCacheService.cs
@@ -0,0 +1,13 @@
+﻿using AddressBookApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookApi.Services
+{
+    public interface IMemoryCacheService
+    {
+        public List<Address> Addresses { get; set; }
+    }
+}
---
+++ AddressBookApi/Services/MemoryCacheService.cs
@@ -0,0 +1,13 @@
+﻿using AddressBookApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookApi.Services
+{
+    public class MemoryCacheService : IMemoryCacheService
+    {
+        public List<Address> Addresses { get; set; } = new List<Address>();
+    }
+}
