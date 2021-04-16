26e88aa1e56b2b5edee096d2f168c37d18382cfc
mcw256 <rivael256@gmail.com>
2021-04-16 23:30:58 +0200

    added swagger and newtonsoft json

--- AddressBookApi/Startup.cs
+++ AddressBookApi/Startup.cs
@@ -1,3 +1,5 @@
+using AddressBookApi.Repositories;
+using AddressBookApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,7 +27,14 @@ namespace AddressBookApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddSingleton<IMemoryCacheService, MemoryCacheService>();
+            services.AddSingleton<IAddressRepo, AddressRepo>();
+
+            services.AddControllers().AddJsonOptions(options => {
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+            });
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +54,14 @@ namespace AddressBookApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
