92e273c9240041c8dd1f42265629084f11a2ceae
mcw256 <rivael256@gmail.com>
2021-04-19 02:19:13 +0200

    tests fixes

--- AddressBookApi.Tests/AddressControllerTests.cs
+++ AddressBookApi.Tests/AddressControllerTests.cs
@@ -5,117 +5,160 @@ using AddressBookApi.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace AddressBookApi.Tests
 {
-    public class AddressControllerTests
+    public class AddressControllerTests : ControllerTestsBase
     {
         private readonly string _baseEndpoint = "https://localhost/api/Address";
 
         [Fact]
         public async Task GetLast_ShouldWork()
         {
-            // Arrange
-            var hostBuilder = new HostBuilder()
-                .ConfigureWebHost(webHost =>
-                {
-                    // Add TestServer
-                    webHost.UseTestServer();
-                    webHost.UseStartup<AddressBookApi.Startup>();
-                });
+            //Arrange
+            await SetupAndRunHost();
 
-            // Create and start up the host
-            var host = await hostBuilder.StartAsync();
+            var firstAddress = new Address()
+            {
+                Id = 1,
+                Name = "a",
+                City = "sdfsdf",
+                Street = "c"
+            };
 
-            // Create an HttpClient which is setup for the test host
-            var client = host.GetTestClient();
+            var secondAddress = new Address()
+            {
+                Id = 2,
+                Name = "q",
+                City = "gdfg",
+                Street = "w"
+            };
+
+            var thirdAddress = new Address()
+            {
+                Id = 3,
+                Name = "y",
+                City = "DDD",
+                Street = "i"
+            };
+
+            _memoryCacheService.Addresses.Add(firstAddress);
+            _memoryCacheService.Addresses.Add(secondAddress);
+            _memoryCacheService.Addresses.Add(thirdAddress);
+
+
+            // Assert
+            var expectedStr = JsonSerializer.Serialize(thirdAddress);
 
             // Act
-            var response = await client.GetAsync(_baseEndpoint);
+            var response = await _httpClient.GetAsync(_baseEndpoint);
+            var actualStr = await response.Content.ReadAsStringAsync();
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(expectedStr, actualStr, true);
         }
 
         [Fact]
         public async Task GetByCity_ShouldWork()
         {
-            // Arrange
-            var hostBuilder = new HostBuilder()
-                .ConfigureWebHost(webHost =>
-                {
-                    // Add TestServer
-                    webHost.UseTestServer();
-                    webHost.UseStartup<AddressBookApi.Startup>();
-                });
+            //Arrange
+            await SetupAndRunHost();
 
-            // Create and start up the host
-            var host = await hostBuilder.StartAsync();
+            var firstAddress = new Address()
+            {
+                Id = 1,
+                Name = "a",
+                City = "LONDON",
+                Street = "c"
+            };
+            
+            var secondAddress = new Address()
+            {
+                Id = 2,
+                Name = "q",
+                City = "LONDON",
+                Street = "w"
+            };
 
-            // Create an HttpClient which is setup for the test host
-            var client = host.GetTestClient();
+            var thirdAddress = new Address()
+            {
+                Id = 3,
+                Name = "y",
+                City = "DDD",
+                Street = "i"
+            };
+
+            _memoryCacheService.Addresses.Add(firstAddress);
+            _memoryCacheService.Addresses.Add(secondAddress);
+            _memoryCacheService.Addresses.Add(thirdAddress);
+
+           
+            // Assert
+            var expectedStr = JsonSerializer.Serialize(new List<Address> { firstAddress, secondAddress } );
 
             // Act
-            var response = await client.GetAsync(_baseEndpoint + "/" + "exampleCity");
+            var response = await _httpClient.GetAsync(_baseEndpoint + "/LONDON");
+            var actualStr = await response.Content.ReadAsStringAsync();
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(expectedStr, actualStr, true);
 
         }
 
         [Fact]
         public async Task GetById_ShouldWork()
         {
-            // Arrange
-            var hostBuilder = new HostBuilder()
-                .ConfigureWebHost(webHost =>
-                {
-                    // Add TestServer
-                    webHost.UseTestServer();
-                    webHost.UseStartup<AddressBookApi.Startup>();
-                });
+            //Arrange
+            await SetupAndRunHost();
 
-            // Create and start up the host
-            var host = await hostBuilder.StartAsync();
+            var firstAddress = new Address()
+            {
+                Id = 1,
+                Name = "a",
+                City = "b",
+                Street = "c"
+            };
 
-            // Create an HttpClient which is setup for the test host
-            var client = host.GetTestClient();
+            _memoryCacheService.Addresses.Add(firstAddress);
+
+            _memoryCacheService.Addresses.Add(new Address()
+            {
+                Id = 2,
+                Name = "q",
+                City = "w",
+                Street = "e"
+            });
+
+            // Assert
+            var expectedStr = JsonSerializer.Serialize(firstAddress);
 
             // Act
-            var response = await client.GetAsync(_baseEndpoint + "/id/" + "4");
+            var response = await _httpClient.GetAsync(_baseEndpoint + "/id/" + "1");
+            var actualStr = await response.Content.ReadAsStringAsync();
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(expectedStr, actualStr, true);
         }
 
         [Fact]
         public async Task Add_ShouldWork()
         {
             // Arrange
-            var hostBuilder = new HostBuilder()
-                .ConfigureWebHost(webHost =>
-                {
-                    // Add TestServer
-                    webHost.UseTestServer();
-                    webHost.UseStartup<AddressBookApi.Startup>();
-                });
-
-            // Create and start up the host
-            var host = await hostBuilder.StartAsync();
-
-            // Create an HttpClient which is setup for the test host
-            var client = host.GetTestClient();
-
-            var content = new StringContent(JsonConvert.SerializeObject(new Address
+            await SetupAndRunHost();
+            var content = new StringContent(JsonSerializer.Serialize(new Address
             {
                 Id = 1,
                 Name = "Aa",
@@ -124,35 +167,22 @@ namespace AddressBookApi.Tests
             }), Encoding.UTF8, "application/json");
 
             // Act
-            var response = await client.PostAsync(_baseEndpoint, content);
+            var response = await _httpClient.PostAsync(_baseEndpoint, content);
 
             dynamic obj = JObject.Parse(await response.Content.ReadAsStringAsync());
 
             // Assert
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-            Assert.NotEqual(0, (int)obj.id);
+            Assert.Single(_memoryCacheService.Addresses);
         }
 
         [Fact]
         public async Task Update_ShouldWork()
         {
             // Arrange
-            var hostBuilder = new HostBuilder()
-                .ConfigureWebHost(webHost =>
-                {
-                    // Add TestServer
-                    webHost.UseTestServer();
-                    webHost.UseStartup<AddressBookApi.Startup>();
-                });
+            await SetupAndRunHost();
 
-            // Create and start up the host
-            var host = await hostBuilder.StartAsync();
-
-            // Create an HttpClient which is setup for the test host
-            var client = host.GetTestClient();
-
-
-            ((IMemoryCacheService)host.Services.GetService(typeof(IMemoryCacheService))).Addresses.Add(new Address()
+            _memoryCacheService.Addresses.Add(new Address()
             {
                 Id = 1,
                 Name = "Aa",
@@ -160,7 +190,7 @@ namespace AddressBookApi.Tests
                 Street = "ww"
             });
 
-            var content = new StringContent(JsonConvert.SerializeObject(new Address
+            var content = new StringContent(JsonSerializer.Serialize(new Address
             {
                 Id = 1,
                 Name = "UPDATED",
@@ -169,33 +199,21 @@ namespace AddressBookApi.Tests
             }), Encoding.UTF8, "application/json");
 
             // Act
-            var response = await client.PutAsync(_baseEndpoint + "/1", content);
+            var response = await _httpClient.PutAsync(_baseEndpoint + "/1", content);
             dynamic obj = JObject.Parse(await response.Content.ReadAsStringAsync());
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Equal("UPDATED", (string)obj.name);
+            Assert.Equal("UPDATED", _memoryCacheService.Addresses.First(a => a.Id == 1).Name);
         }
 
         [Fact] 
         public async Task Delete_ShouldWork()
         {
-            // Arrange
-            var hostBuilder = new HostBuilder()
-                .ConfigureWebHost(webHost =>
-                {
-                    // Add TestServer
-                    webHost.UseTestServer();
-                    webHost.UseStartup<AddressBookApi.Startup>();
-                });
-
-            // Create and start up the host
-            var host = await hostBuilder.StartAsync();
-
-            // Create an HttpClient which is setup for the test host
-            var client = host.GetTestClient();
-
-            ((IMemoryCacheService)host.Services.GetService(typeof(IMemoryCacheService))).Addresses.Add(new Address()
+            // Arrange 
+            await SetupAndRunHost();
+            _memoryCacheService.Addresses.Add(new Address()
             {
                 Id = 1,
                 Name = "Aa",
@@ -204,10 +222,11 @@ namespace AddressBookApi.Tests
             });
 
             // Act
-            var response = await client.DeleteAsync(_baseEndpoint + "/1");
+            var response = await _httpClient.DeleteAsync(_baseEndpoint + "/1");
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Empty(_memoryCacheService.Addresses);
         }
 
     }
---
+++ AddressBookApi.Tests/ControllerTestsBase.cs
@@ -0,0 +1,41 @@
+﻿using AddressBookApi.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookApi.Tests
+{
+    public abstract class ControllerTestsBase
+    {
+        protected IHost _host;
+        
+        protected HttpClient _httpClient;
+
+        protected IMemoryCacheService _memoryCacheService;
+
+        protected async Task SetupAndRunHost()
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHost =>
+                {
+                    // Add TestServer
+                    webHost.UseTestServer();
+                    webHost.UseStartup<AddressBookApi.Startup>();
+                });
+
+            // Create and start up the host
+            _host = await hostBuilder.StartAsync();
+
+            // Create an HttpClient which is setup for the test host
+            _httpClient = _host.GetTestClient();
+
+            // Create service handle
+            _memoryCacheService = ((IMemoryCacheService)_host.Services.GetService(typeof(IMemoryCacheService)));
+        }
+    }
+}
